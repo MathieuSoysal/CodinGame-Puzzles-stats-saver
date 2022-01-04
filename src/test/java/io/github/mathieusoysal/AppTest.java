9f955f600f826fcc320ca78b70b7f00256c94990
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2022-01-04 14:23:41 +0100

    ✨ add new features : get properties from env

--- src/test/java/io/github/mathieusoysal/AppTest.java
+++ src/test/java/io/github/mathieusoysal/AppTest.java
@@ -1,13 +1,15 @@
 package io.github.mathieusoysal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
 
 }

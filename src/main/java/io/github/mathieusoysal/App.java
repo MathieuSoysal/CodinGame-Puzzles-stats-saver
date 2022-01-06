93c59da1309df26f6ac48dd7483e9218738a83b0
Mathieu Soysal <Mathieu.Soysal@etu.umontpellier.com>
2022-01-06 20:40:33 +0100

    🔨 Add App class

--- src/main/java/io/github/mathieusoysal/App.java
+++ src/main/java/io/github/mathieusoysal/App.java
@@ -1,8 +1,25 @@
 package io.github.mathieusoysal;
 
+import java.util.List;
+
+import com.github.mathieusoysal.codingame_stats.CodinGame;
+import com.github.mathieusoysal.codingame_stats.puzzle.Puzzle;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import io.github.mathieusoysal.dao.PuzzlesDao;
+import io.github.mathieusoysal.resources.ApplicationProperties;
+
 public class App {
-    
+
     public static void main(String[] args) {
+        try (MongoClient mongoClient = MongoClients.create(new ConnectionString(
+                ApplicationProperties.MONGODB_URI))) {
+            PuzzlesDao puzzlesDao = new PuzzlesDao(mongoClient, ApplicationProperties.MONGODB_DATABASE);
+            List<Puzzle> puzzles = new CodinGame().getPuzzles();
+            puzzlesDao.saveAll(puzzles);
+        }
     }
-    
+
 }

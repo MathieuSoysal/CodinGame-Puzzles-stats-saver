4fb0a98124093df70abbc00947f04dd10accff8f
Mathieu Soysal <Mathieu.Soysal@etu.umontpellier.fr>
2022-01-23 22:10:48 +0100

    🔨 Add docker-secret managment

--- src/main/java/io/github/mathieusoysal/resources/ApplicationProperties.java
+++ src/main/java/io/github/mathieusoysal/resources/ApplicationProperties.java
@@ -1,25 +1,59 @@
 package io.github.mathieusoysal.resources;
 
+import java.util.Map;
+
+import com.cars.framework.secrets.DockerSecretLoadException;
+import com.cars.framework.secrets.DockerSecrets;
+
+import org.apache.logging.log4j.Logger;
+
+import io.github.mathieusoysal.exceptions.DockerSecretVariableNotFoundException;
 import io.github.mathieusoysal.exceptions.EnvironmentVariableNotFoundException;
 
 public class ApplicationProperties {
+    private static final Logger LOGGER = org.apache.logging.log4j.LogManager.getLogger(ApplicationProperties.class);
+
     private ApplicationProperties() {
         throw new IllegalStateException("Utility class");
     }
 
-    private static final String MONGODB_PASSWORD = getenv("MONGODB_PASSWORD");
-    private static final String MONGODB_USERNAME = getenv("MONGODB_USERNAME");
-    private static final String MONGODB_ADDRESS = getenv("MONGODB_ADDRESS");
-    public static final String MONGODB_DATABASE = getenv("MONGODB_DATABASE");
+    private static final Map<String, String> DOCKER_SECRETS = getDockerSecrets();
+    private static final String MONGODB_PASSWORD = getProperty("MONGODB_PASSWORD");
+    private static final String MONGODB_USERNAME = getProperty("MONGODB_USERNAME");
+    private static final String MONGODB_ADDRESS = getProperty("MONGODB_ADDRESS");
+    public static final String MONGODB_DATABASE = getProperty("MONGODB_DATABASE");
     public static final String MONGODB_URI = String.format("mongodb+srv://%s:%s@%s",
             MONGODB_USERNAME,
             MONGODB_PASSWORD,
             MONGODB_ADDRESS);
 
-    private static String getenv(final String name) {
-        var value = System.getenv(name);
-        if (value == null)
+    private static Map<String, String> getDockerSecrets() {
+        try {
+            return DockerSecrets.loadFromFile("mongodb-secret");
+        } catch (DockerSecretLoadException e) {
+            LOGGER.info(
+                    "Could not load docker secrets mongodb-secret, search if MongoDB properties are on environment variables");
+            return null;
+        }
+    }
+
+    private static String getProperty(final String name) {
+        if (DOCKER_SECRETS != null)
+            return getPropertyFromDockerSecret(name);
+        return getPropertyFromEnvironmentVariable(name);
+    }
+
+    private static String getPropertyFromEnvironmentVariable(final String name) {
+        var property = System.getenv(name);
+        if (property == null)
+            throw new DockerSecretVariableNotFoundException(name);
+        return property;
+    }
+
+    private static String getPropertyFromDockerSecret(final String name) {
+        String property = DOCKER_SECRETS.get(name);
+        if (property == null)
             throw new EnvironmentVariableNotFoundException(name);
-        return System.getenv(name);
+        return property;
     }
 }
